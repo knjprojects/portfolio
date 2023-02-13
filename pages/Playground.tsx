d728a86d7db8fa74984eb4b08aa1a65622acad91
Joshua Bridgemohan <DELL@MacBook-Pro.local>
2023-02-12 22:55:24 -0400

    first commit

---
+++ pages/Playground.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+type Props = {}
+
+const Playground = (props: Props) => {
+  return (
+    <div>Playground  
+      <div className='flex-row '></div>
+    </div>
+  
+  )
+}
+
+export default Playground
